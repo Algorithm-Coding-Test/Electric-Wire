ea7f1db4ca0aac83caab4e5ef687a8134a7cba4d
aqaqsubin <aqaq925120k@naver.com>
2020-08-15 13:47:46 +0900

    Finish, using LIS Algorithm & Memorization

--- Main.java
+++ Main.java
@@ -1,22 +1,6 @@
 import java.util.*;
 
 public class Main {
-    public static class Line{
-        int pointA, pointB;
-
-        public Line(int pointA, int pointB){
-            this.pointA = pointA;
-            this.pointB = pointB;
-        }
-    }
-    public static class Value{
-        int firstNum;
-        int value;
-        public Value(int firstNum, int value){
-            this.firstNum = firstNum;
-            this.value = value;
-        }
-    }
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -25,49 +9,50 @@ public class Main {
         scan.nextLine();
 
         int pointA, pointB;
-        ArrayList<Line> lines = new ArrayList<>();
+        Map<Integer, Integer> lines = new HashMap<Integer, Integer>();
+        
         for(int i=0;i<N;i++){
             pointA = scan.nextInt();
             pointB = scan.nextInt();
+            
+            lines.put(pointA, pointB);
 
-            lines.add(new Line(pointA, pointB));
             scan.nextLine();
         }
-
         // Sort by A point
-        Collections.sort(lines, new Comparator<Line>(){
-            @Override
-            public int compare(Line line1, Line line2){
-                if(line1.pointA > line2.pointA) return 1;
-                else if(line1.pointA == line2.pointA) return 0;
-                return -1;
-            }
-        });
-        
+        TreeMap<Integer,Integer> tree = new TreeMap<Integer,Integer>(lines);
+        Iterator<Integer> iteratorKey = tree.keySet( ).iterator( );
+      
         ArrayList<Integer> bPoints = new ArrayList<>();
         
-        for(int i=0;i<N;i++) bPoints.add(lines.get(i).pointB);
+        while(iteratorKey.hasNext()) bPoints.add(lines.get(iteratorKey.next()));
         
-        System.out.println(N-LIS(bPoints));
+        int[] map = new int[N];
+        int tmpMax;
+        int maxNum =0;
+        
+        // Apply LIS Algorithm at B sequence
+
+        for(int i =0;i<N;i++) map[i] = 1;
+        
+        for(int i = 1;i < N;i ++) {
+        	tmpMax = map[i];
+        	for(int j = i-1;j >= 0;j--) {
+        		if(bPoints.get(j) < bPoints.get(i) && tmpMax <= map[j]) {
+        			tmpMax = map[j]+1;
+        		}
+        	}
+        	map[i] = tmpMax;
+        }
+        
+        for(int i = 0;i < N;i++) {
+    		maxNum = (maxNum < map[i]) ? map[i] : maxNum;
+    	}
+        System.out.println(N-maxNum);
         
 
     }
-    // Apply LIS Algorithm at B sequence
-    public static int LIS(ArrayList<Integer> arr){
-    	int returnLength = 0;
-    	ArrayList<Integer> tmpArr;
-    	
-    	if (arr.size() == 1) return 1;
-    	
-    	for(int i= 0;i<arr.size();i++) {
-    		tmpArr = new ArrayList<>();
-    		
-    		for(int j=i+1;j<arr.size();j++) {
-    			if(arr.get(i) < arr.get(j)) tmpArr.add(arr.get(j));
-    		}
-    		returnLength = Math.max(returnLength,1+LIS(tmpArr));
-    	}
-    	return returnLength;
-    }
+    
+    
     
 }
